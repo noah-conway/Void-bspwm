b38abe50a18450342a3bd695a0b11ed99010228d
Noah <nconway@posteo.net>
2019-07-20 15:36:49 -0700

    first commit

---
+++ install-packages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#xbps-install -S xorg xorg-fonts xf86-video-intel gcc libgfortran-devel pkg-config libX11-devel libXft-devel libXinerama-devel fontconfig-devel ncurses-term st-terminfo vim git make openssh oath-toolkit R dmenu bspwm sxhkd zathura mpc mpd ncmpcpp 
+
+#if [ ! -d "~/.config" ]; then
+#	mkdir .config;
+#fi
+cd /home/noah/.config
+#git clone https://git.suckless.org/st
